41918cd6f1bfae5638a05ab69601d440067187a2
ismaelmjr <59127089+ismaelmjr@users.noreply.github.com>
2021-11-27 14:08:10 -0600

    Updated movie card and created styles file

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,4 +1,5 @@
-import React from "react"; //imported react to the component file.
+import React from "react"; //imported react to the movie card file.
+import PropTypes from "prop-types"; // import proptypes to the movie card. 
 
 class MovieCard extends React.Component { //created a component called moviecard.
     render() {
@@ -6,5 +7,25 @@ class MovieCard extends React.Component { //created a component called moviecard
         return <div className ="movie-card" onClick={() => {onMovieClick(movie);}}>{movie.Title}</div>;
     }
 }
+
+MovieCard.propTypes = {
+    movie: PropTypes.shape({
+        Title:  PropTypes.string.isRequired,
+        Description: PropTypes.string.isRequired,
+        Genre: PropTypes.shape({
+            Name: PropTypes.string.isRequired,
+            Description: PropTypes.string.isRequired
+        }),
+        Director: PropTypes.shape({
+            Name: PropTypes.string.isRequired,
+            Bio: PropTypes.string.isRequired,
+            Birth: PropTypes.string.isRequired,
+            Death: PropTypes.string
+        }),
+        ImagePath: PropTypes.string.isRequired,
+        Featured: PropTypes.bool
+    }).isRequired,
+    onMovieClick: PropTypes.func.isRequired
+};
 
 export default MovieCard;
