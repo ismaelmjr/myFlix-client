bcb8d03a1d1ce102465ddf4f60e585920346fa7d
ismaelmjr <59127089+ismaelmjr@users.noreply.github.com>
2021-11-27 14:08:27 -0600

    updated movie view and created styles file

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,12 +1,7 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 class MovieView extends React.Component {
-
-    componentDidMount(){
-        document.addEventListener('keypress', event => {
-            console.log(event.key);
-        });
-    }
 
     render(){
         const {movie, onBackClick} = this.props;
@@ -26,16 +21,44 @@ class MovieView extends React.Component {
                 </div>
                 <div className="movie-genre">
                     <span className="label">Genre: </span>
-                    <span className="value">{movie.Genre}</span>
+                    <span className="value">{movie.Genre.Name}</span>
                 </div>
-                <div className="movie-director">
+                <div className="director-name">
                     <span className="label">Director: </span>
-                    <span className="value">{movie.Director}</span>
+                    <span className="value">{movie.Director.Name}</span>
+                </div>
+                <div className="director-bio">
+                    <span className="label">Bio: </span>
+                    <span className="value">{movie.Director.Bio}</span>
+                </div>
+                <div className="director-birth">
+                    <span className="label">Birth: </span>
+                    <span className="value">{movie.Director.Birth}</span>
                 </div>
                 <button onClick= {() => {onBackClick(null); }} >Back</button>
             </div>
         );
     }
 }
+
+MovieView.propTypes = {
+    movie: PropTypes.shape({
+        Title:  PropTypes.string.isRequired,
+        Description: PropTypes.string.isRequired,
+        Genre: PropTypes.shape({
+            Name: PropTypes.string.isRequired,
+            Description: PropTypes.string.isRequired
+        }),
+        Director: PropTypes.shape({
+            Name: PropTypes.string.isRequired,
+            Bio: PropTypes.string.isRequired,
+            Birth: PropTypes.string.isRequired,
+            Death: PropTypes.string
+        }),
+        ImagePath: PropTypes.string.isRequired,
+        Featured: PropTypes.bool
+    }).isRequired,
+    onBackClick: PropTypes.func.isRequired
+};
 
 export default MovieView;
